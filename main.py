07420e69787500e4548142db589355e9c5063691
Brady Anderson <bradya48@gmail.com>
2017-03-15 19:31:22 +0000

    refactored functions using list comprehension

--- main.py
+++ main.py
@@ -1,94 +1,59 @@
 def print_grid():
 	for row in grid:
 		if grid.index(row) % 3 == 0:
-			print ""
-		print "%s %s %s  %s %s %s  %s %s %s" %  tuple(row[i] for i in range(9))
+			print
+		print "%s %s %s  %s %s %s  %s %s %s" % tuple(row[i] for i in range(9))
+
 
 def check_complete():
-	for i in range(9):
-		if 0 in grid[i]:
-			return False
+	if len([i for i in range(9) if 0 in grid[i]]) != 0:
+		return False
 	return True
 
 
-
-
 def find_possible(x,y):
-	poss_values = []
-	row_poss = check_row(x,y)
+	row_poss = check_row(y)
 	col_poss = check_col(x,y)
 	box_poss = check_box(x,y)
 
-	for value in row_poss:
-		if value in col_poss:
-			if value in box_poss:
-				poss_values.append(value)
-
+	poss_values = (row_poss & col_poss & box_poss)
 	if len(poss_values) == 1:
-		return poss_values[0]
-	else:
-		return 0
+		return poss_values.pop()
+	return 0
 
-def check_row(x,y):
-	row_poss = []
 
-	for i in range(1,10):
-		if i not in grid[y]:
-			row_poss.append(i)
-
-	return row_poss
+def check_row(y):
+	return set([i for i in range(1,10) if i not in grid[y]])
 
 
 def check_col(x,y):
-	col = []
-	col_poss = []
-
-	for i in range(9):
-		col.append(grid[i][x])
-
-	for i in range(1,10):
-		if i not in col:
-			col_poss.append(i)
-
-	return col_poss
+	col = [grid[i][x] for i in range(9)]
+	return set([i for i in range(1,10) if i not in col])
 
 
 def check_box(x,y):
-	box = []
-	box_poss = []
-
 	box_x = x // 3
 	box_y = y // 3
 
-	for i in range(3):
-		for j in range(3):
-			box.append(grid[(box_y * 3) + i][(box_x * 3) + j])
-
-	for i in range(1,10):
-		if i not in box:
-			box_poss.append(i)
-
-	return box_poss
+	box = [grid[(box_y * 3) + i][(box_x * 3) + j] for j in range(3) for i in range(3)]
+	return set([i for i in range(1,10) if i not in box])
 
 
-#grid = [[0] * 9] * 9
-grid = 	[[0, 2, 0,    9, 4, 0,    0, 0, 3],
-		 [9, 0, 6,    0, 2, 3,    0, 0, 8],
-		 [0, 3, 0,    0, 0, 8,    0, 7, 2],
+grid = 	[[0, 0, 0,    0, 0, 9,    0, 4, 7],
+		 [0, 9, 8,    7, 0, 6,    0, 1, 3],
+		 [0, 7, 0,    0, 0, 0,    0, 8, 0],
 
-		 [0, 0, 0,    0, 0, 0,    0, 0, 5],
-		 [3, 6, 1,    5, 0, 9,    2, 4, 7],
-		 [8, 0, 0,    0, 0, 0,    0, 0, 0],
+		 [8, 0, 7,    0, 5, 1,    0, 0, 4],
+		 [5, 0, 0,    0, 6, 0,    0, 0, 8],
+		 [9, 0, 0,    8, 7, 0,    2, 0, 1],
 
-		 [1, 5, 0,    8, 0, 0,    0, 3, 0],
-		 [6, 0, 0,    3, 7, 0,    5, 0, 1],
-		 [4, 0, 0,    0, 5, 2,    0, 6, 0]]
+		 [0, 4, 0,    0, 0, 0,    0, 2, 0],
+		 [1, 5, 0,    2, 0, 7,    8, 3, 0],
+		 [6, 8, 0,    1, 0, 0,    0, 0, 0]]
 
-
-poss_grid = [[[]] * 9] * 9
 complete = False
 
-while complete == False:
+while not complete:
 
 	for y in range(9):
 		for x in range(9):
